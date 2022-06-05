16353c5e506195bb0c3368f8742f1f9d94965885
Kemunto Gift <giftoyugi@gmail.com>
2022-06-05 18:04:53 +0300

    add logic to eventListAdapter for individual views

---
+++ app/src/main/java/com/kemunto/nofomo/adapters/EventListAdapter.java
@@ -0,0 +1,68 @@
+package com.kemunto.nofomo.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kemunto.nofomo.R;
+import com.kemunto.nofomo.models.Event;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventViewHolder> {
+    private List<Event> mOccasions;
+    private Context mContext;
+
+    public EventListAdapter(Context context, List<Event> occasions) {
+        mContext = context;
+        mOccasions = occasions;
+    }
+
+    @Override
+    public EventListAdapter.EventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_list_item, parent, false);
+        EventViewHolder viewHolder = new EventViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(EventListAdapter.EventViewHolder holder, int position) {
+        holder.bindOccasion(mOccasions.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mOccasions.size();
+    }
+
+    public class EventViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.eventImageView)
+        ImageView mEventImageView;
+        @BindView(R.id.eventTitleTextView)
+        TextView mEventTitleTextView;
+        @BindView(R.id.performersTextView) TextView mPerformersTextView;
+        @BindView(R.id.statsTextView) TextView mStatsTextView;
+
+        private Context mContext;
+
+        public EventViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindOccasion(Event occasion) {
+            mEventTitleTextView.setText(occasion.getTitle());
+            mPerformersTextView.setText(occasion.getPerformers().get(0).getName());
+            mStatsTextView.setText("Stats: " + occasion.getStats());
+        }
+    }
+}
