dba441fcbbedd35ec3685b8965349b6b735d187e
Kemunto Gift <giftoyugi@gmail.com>
2022-06-05 13:11:15 +0300

    sync project files to enable butterknife binding

--- app/src/main/java/com/kemunto/nofomo/MainActivity.java
+++ app/src/main/java/com/kemunto/nofomo/MainActivity.java
@@ -3,12 +3,22 @@ package com.kemunto.nofomo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.searchButton)
+    Button mSearchButton;
+    @BindView(R.id.eventEditText)
+    EditText mEventEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
