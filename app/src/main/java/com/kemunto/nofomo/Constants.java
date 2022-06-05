c0a62176b3660560fe4debac3aa90d15dc5d3d73
Kemunto Gift <giftoyugi@gmail.com>
2022-06-05 15:23:50 +0300

    add API key and constants class with instantiated string variables

---
+++ app/src/main/java/com/kemunto/nofomo/Constants.java
@@ -0,0 +1,6 @@
+package com.kemunto.nofomo;
+
+public class Constants {
+    public static final String SEAT_BASE_URL = "https://api.seatgeek.com/2/";
+    public static final String SEAT_API_KEY = BuildConfig.SEAT_API_KEY;
+}
