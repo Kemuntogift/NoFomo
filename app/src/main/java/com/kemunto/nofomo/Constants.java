8e1bf09db0c2cf4c870dc3fd2c9d98367aff8791
Kemunto Gift <giftoyugi@gmail.com>
2022-06-12 21:46:14 +0300

    refactor EventListACtivity

--- app/src/main/java/com/kemunto/nofomo/Constants.java
+++ app/src/main/java/com/kemunto/nofomo/Constants.java
@@ -3,6 +3,7 @@ package com.kemunto.nofomo;
 public class Constants {
     public static final String SEAT_BASE_URL = "https://api.seatgeek.com/2/";
     public static final String SEAT_API_KEY = BuildConfig.SEAT_API_KEY;
+    public static final String PREFERENCES_TYPE_KEY = "type";
     public static final String FIREBASE_CHILD_SEARCHED_TYPE = "searchedType";
     public static final String FIREBASE_CHILD_OCCASIONS = "occasions";
 }
--- app/src/main/java/com/kemunto/nofomo/EventListActivity.java
+++ app/src/main/java/com/kemunto/nofomo/EventListActivity.java
@@ -5,9 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.kemunto.nofomo.adapters.EventListAdapter;
@@ -25,6 +32,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class EventListActivity extends AppCompatActivity {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentChoice;
+
     private static final String TAG = EventListActivity.class.getSimpleName();
     private EventListAdapter mAdapter;
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
@@ -39,42 +50,47 @@ public class EventListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_list);
         ButterKnife.bind(this);
 
-        Intent intent = getIntent();
-        String type = intent.getStringExtra("type");
-
-        SeatApi client = SeatClient.getClient();
-
-        Call<SeatGeekEventsSearchResponse> call = client.getOccasions(type);
-
-        call.enqueue(new Callback<SeatGeekEventsSearchResponse>() {
-            @Override
-            public void onResponse(Call<SeatGeekEventsSearchResponse> call, Response<SeatGeekEventsSearchResponse> response) {
-                hideProgressBar();
-
-                if (response.isSuccessful()) {
-                    occasions = response.body().getEvents();
-                    mAdapter = new EventListAdapter(EventListActivity.this, occasions);
-
-                    RecyclerView.LayoutManager layoutManager =
-                            new LinearLayoutManager(EventListActivity.this);
-                    mRecyclerView.setLayoutManager(layoutManager);
-                    mRecyclerView.setHasFixedSize(true);
-                    mRecyclerView.setAdapter(mAdapter);
-                    showEvents();
-                } else {
-                    showUnsuccessfulMessage();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SeatGeekEventsSearchResponse> call, Throwable t) {
-                hideProgressBar();
-                showFailureMessage();
-            }
-
-        });
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentChoice = mSharedPreferences.getString(Constants.PREFERENCES_TYPE_KEY, null);
+        if(mRecentChoice != null){
+            fetchOccasions(mRecentChoice);
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String type) {
+                addToSharedPreferences(type);
+                fetchOccasions(type);
+                return false;
+            }
+
+
+            @Override
+            public boolean onQueryTextChange(String type) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return super.onOptionsItemSelected(item);
+    }
 
     private void showFailureMessage() {
         mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
@@ -91,7 +107,47 @@ public class EventListActivity extends AppCompatActivity {
     }
 
     private void hideProgressBar() {
-
         mProgressBar.setVisibility(View.GONE);
     }
+
+    private void addToSharedPreferences(String type) {
+        mEditor.putString(Constants.PREFERENCES_TYPE_KEY, type).apply();
+    }
+
+
+    private void fetchOccasions(String type){
+        SeatApi client = SeatClient.getClient();
+
+        Call<SeatGeekEventsSearchResponse> call = client.getOccasions(type);
+
+        call.enqueue(new Callback<SeatGeekEventsSearchResponse>() {
+            @Override
+            public void onResponse(Call<SeatGeekEventsSearchResponse> call, Response<SeatGeekEventsSearchResponse> response) {
+                hideProgressBar();
+
+                if (response.isSuccessful()) {
+                    occasions = response.body().getEvents();
+                    mAdapter = new EventListAdapter(EventListActivity.this, occasions);
+
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager =
+                            new LinearLayoutManager(EventListActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+                    showEvents();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SeatGeekEventsSearchResponse> call, Throwable t) {
+                Log.e(TAG, "onFailure: ",t );
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
+    }
+
 }
